5da68a1003a04b92f8d4653d3db66449e2bd2c86
David Greengas <dave.greengas@gmail.com>
2025-10-28 13:48:09 -0500

    update libfprint to come from fedora 42

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,7 @@ dnf5 install -y tmux
 # Install libfprint with Elan SoC support from COPR
 echo "Enabling copr gaanee/libfprint-elanmoc2"
 #dnf5 -y copr enable  ntibbitts/Libfprint-Elanmoc2 
-dnf -y copr enable gaanee/libfprint-elanmoc2
+dnf -y copr enable gaanee/libfprint-elanmoc2 fedora-42-x86_64
 echo "install libfprint"
 dnf5 -y install libfprint-elanmoc2
 # Disable COPRs so they don't end up enabled on the final image:
